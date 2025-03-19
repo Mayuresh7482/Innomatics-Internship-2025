0c7007e4d84b2fbc0321fa27ba5f899143b3f152
Mayuresh7482 <boratemayuresh757@gmail.com>
2025-03-19 16:15:14 +0530

    Added Nodejs files and gitignore

---
+++ Nodejs/f.js
@@ -0,0 +1,50 @@
+// studentdetials={
+//       name:"Raj",
+//       age:20,
+//       grade:"A"
+
+// }
+
+// function smart(){
+//       console.log("I am smart");
+//       return true;
+// }
+// function hard(){
+//       console.log("I am hardworking");
+// }
+
+// module.exports={studentdetials, smart, hard};
+
+// const os=require("os");
+// console.log(os.totalmem());
+// console.log(os.freemem());
+// console.log(os.hostname());
+// console.log(os.platform());
+// console.log(os.type());
+// console.log(os.userInfo());
+// console.log(os.version());
+// console.log(os.uptime());
+// console.log(os.release());
+// console.log(os.tmpdir());
+// module.exports=os;
+
+// const { log } = require("console");
+// const path = require("path");
+// const a = path.basename("C:UsersacerOneDriveDesktophappyholi.mp4");
+// const b = path.extname("C:UsersacerOneDriveDesktophappyholi.mp4");
+
+// console.log(a);
+// console.log(b);
+// console.log(__dirname);
+
+const http =require('http');
+const port=4000;
+const server = http.createServer((req,res)=>{
+      res.write("Hello World");
+      res.end();
+}
+)
+
+server.listen(port,()=>{
+      console.log(`Server running at http://localhost:${port}`);
+})
